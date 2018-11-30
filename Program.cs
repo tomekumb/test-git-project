a92aaa683ef382555657ca0d9a6fa61d058de07a
ITMAGINATION\tomasz.umbras <tomasz.umbras@itmagination.com>
2018-11-30 15:52:47 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestGitProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter google phrase: ");
+            var phrase = Console.ReadLine();
+            Console.WriteLine("Connectring with Google...");
+
+        }
+    }
+}
